5efd627444b9f5096f8537af63e9d97debb097a0
sanjay_uzan <sanjaykhadka@Sanjays-MacBook-Pro.local>
2026-04-21 09:44:58 -0400

    features: project setup,nativewind,routing,tab-navigation

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { styled } from "nativewind";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView); //SafeAreaView is a third-party component, so nativewind needs style wrapper to enable classname support on it.
 
 const Insights = () => {
   return (
-    <View>
+     <SafeAreaView className="flex-1 bg-background p-5">
       <Text>Insights</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
