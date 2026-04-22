f7eac2bea7f888b436d645c16f004b3cb85c54dd
sanjay_uzan <sanjaykhadka@Sanjays-MacBook-Pro.local>
2026-04-22 09:56:57 -0400

    New Fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,40 +9,38 @@ const SafeAreaView = styled(RNSafeAreaView); //SafeAreaView is a third-party com
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-7xl font-sans-extrabold">Home</Text>
       <Link
         href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Onboarding
       </Link>
       <Link
         href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sign In
       </Link>
       <Link
         href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sign Up
       </Link>
 
       {/* static routing */}
-      <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+      {/* <Link href="/subscriptions/spotify">Spotify Subscription</Link> */}
 
       {/* dynamic routing through id */}
-      <Link
+      {/* <Link
         href={{
           pathname: "/subscriptions/[id]",
           params: { id: "claude" },
-        }}
+        }} 
       >
         Claude Max Subscription
-      </Link>
+      </Link> */}
     </SafeAreaView>
   );
 }
